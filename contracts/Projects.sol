15ac99cc0fb1fc0b96be6af48d3585a9136aea57
nikhil <nikhilparihar0908@gmail.com>
2025-02-10 19:42:44 +0530

    Update Validator.sol

--- contracts/Projects.sol
+++ contracts/Projects.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
-import "./validator.sol";  
+import "./Validator.sol";  
 
 contract ProjectSelector {
     ValidatorRegistry public validatorRegistry;
