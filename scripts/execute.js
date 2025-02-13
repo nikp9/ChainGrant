850fde6f9268be3ec40773196378859c62e7b341
nikhil <nikhilparihar0908@gmail.com>
2025-02-14 00:05:13 +0530

    Add execute.js

---
+++ scripts/execute.js
@@ -0,0 +1,79 @@
+const hre = require("hardhat");
+const fs = require("fs");
+
+async function main() {
+    addresses = JSON.parse(fs.readFileSync("addresses.json", "utf-8"));
+    const EntryPoint = await hre.ethers.getContractAt("EntryPoint", addresses["EntryPoint"]);
+    const Accounts = await hre.ethers.getContractAt("Accounts", addresses["Accounts"]);
+    const Paymaster = await hre.ethers.getContractAt("Paymaster", addresses["Paymaster"]);
+    const ProjectDetails = await hre.ethers.getContractAt("ProjectDetails", addresses["ProjectDetails"]);
+
+    // Get contract instances
+    const [signer] = await hre.ethers.getSigners();
+
+    // Get the current nonce
+    const nonce = await EntryPoint.getNonce(Accounts.target, 0);
+    console.log("Current nonce:", nonce);
+
+    // Encoding the function to call
+    const targetFunction = ProjectDetails.interface.encodeFunctionData("addProjectDetails", [1]);
+
+    // Prepare the execute call
+    const functionCallData = Accounts.interface.encodeFunctionData("execute", [ProjectDetails.target, 0, targetFunction]);
+    
+    const userOp = {
+        sender: Accounts.target,
+        nonce: nonce,
+        initCode: "0x",
+        callData: functionCallData,
+        callGasLimit: 200_000,
+        verificationGasLimit: 200_000,
+        preVerificationGas: 50_000,
+        maxFeePerGas: hre.ethers.parseUnits("10", "gwei"),
+        maxPriorityFeePerGas: hre.ethers.parseUnits("5", "gwei"),
+        paymasterAndData: Paymaster.target,
+        signature: "0x"
+    };
+
+    // console.log("UserOperation:", userOp);
+
+    try {
+        // Estimate gas for the handleOps call
+        const gasLimit = await EntryPoint.handleOps.estimateGas(
+            [userOp],
+            signer
+        );
+        // console.log("Estimated gas limit:", gasLimit);
+
+        // Send the operation
+        const tx = await EntryPoint.handleOps(
+            [userOp],
+            signer,
+            {
+                gasLimit: gasLimit * 2n
+            }
+        );
+        console.log("Transaction hash:", tx.hash);
+        
+        const receipt = await tx.wait();
+        // console.log("Transaction Receipt:", receipt);
+
+        // Verify the count was incremented
+        const update = await ProjectDetails.idToProjectDetails(1);
+        console.log("Response :", update);
+    } catch (error) {
+        console.error("Error executing UserOperation:");
+        if (error.data) {
+            // Try to decode the revert reason
+            console.error("Revert reason:", error.data);
+        }
+        throw error;
+    }
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
