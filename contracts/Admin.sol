e5656428170c66e473c8d670ded1eb2be4a43428
T-Rishabh <shuklarishu036@gmail.com>
2025-03-08 00:02:07 +0530

    Updated

--- contracts/Admin.sol
+++ contracts/Admin.sol
@@ -1,19 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
+// Simple admin management contract
 contract Admin {
+    // Super admin address - cannot be changed after deployment
     address public immutable admin;
+    
+    uint256 public Total_milestone;
+   
+    // Tracks admin status: 0 = not admin, 1 = admin
     mapping(address => uint8) public admins;
 
+    // Sets contract deployer as initial super admin
     constructor() {
         admin = msg.sender;
         admins[msg.sender] = 1;
     }
 
+     function add_TotalMilestone(uint256 M) public{
+        Total_milestone=M;
+     }
+
+    // Allows super admin to add new admins
     function addAdmin(address _newAdmin) public {
         require(msg.sender == admin, "Only super admin can add admins");
         admins[_newAdmin] = 1;
     }
 
+    // Checks if an address is an admin
     function isAdmin(address _address) public view returns (bool) {
         return admins[_address] == 1;
     }
--- contracts/FundTransfer.sol
+++ contracts/FundTransfer.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "./Project.sol";
-import "./Validator.sol";
+import "./Projects.sol";
+import "./validator.sol";
 
 contract Fund_tracking {
     Admin public adminContract;
