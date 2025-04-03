3a25c91f126398e8b158387866643901667096b3
nikhil <nikhilparihar0908@gmail.com>
2025-04-04 00:43:47 +0530

    Add help page

---
+++ frontend/src/components/Help.jsx
@@ -0,0 +1,15 @@
+import ValidationForm from "./ValidationForm.jsx";
+
+function Help() {
+    return (
+        <div className="bg-[#141c2e] min-h-screen text-white pt-16">
+            <div className="bg-[#141c2e] pb-10">
+                <div className="max-w-7xl mx-auto px-4 py-6">
+                    <ValidationForm/>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Help;
