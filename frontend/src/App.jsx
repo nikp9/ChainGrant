92f8bf880db059a212e852363a826e0f959dd163
nikhil <nikhilparihar0908@gmail.com>
2025-04-07 00:39:39 +0530

    Add routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar.jsx'
 import Home from './components/Home.jsx'
+import { Outlet } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,7 +9,7 @@ function App() {
   return (
     <>
       <Navbar/>
-      <Home/>
+      <Outlet/>
     </>
   )
 }
