aeb8e87e0bc38708637e91ee768f601da9205838
nikhil <nikhilparihar0908@gmail.com>
2025-04-10 00:14:26 +0530

    Add routing for wallet guide

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,42 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import PageLayout from './components/PageLayout.jsx'
+import Home from './components/Home.jsx'
+import WalletGuide from './components/WalletGuide.jsx'
+import ProjectPage from './components/ProjectPage.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+    children: [
+      {
+        path: '',
+        element: <Home/>
+      },
+      {
+        path: 'guide',
+        element: <WalletGuide/>
+      },
+      {
+        path: 'projects',
+        element: <PageLayout/>,
+        children: [
+          {
+            path: ':id',
+            element: <ProjectPage/>
+          }
+        ]
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />
   </StrictMode>,
 )
