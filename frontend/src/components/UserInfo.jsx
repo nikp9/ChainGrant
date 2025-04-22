d5d8833adee7e08612231daa05cba71c818328d3
nikhil <nikhilparihar0908@gmail.com>
2025-04-22 21:56:28 +0530

    Update UserInfo

--- frontend/src/components/UserInfo.jsx
+++ frontend/src/components/UserInfo.jsx
@@ -3,8 +3,7 @@ import { useState } from "react";
 function UserInfo(){
     return(
     <>
-        <h1 className="color: bg-red-100">UserInfo</h1>
-        <h2 className="flex flex-auto">Dropdown content</h2>
+
     </>
     )
 }
