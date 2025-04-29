3b37f7764ba167681b4d7cd5f523b29c51d2cc82
nikhil <nikhilparihar0908@gmail.com>
2025-04-29 23:37:14 +0530

    Update deployment script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,37 +5,7 @@ async function main(){
     const EntryPoint = await hre.ethers.deployContract("EntryPoint");
     await EntryPoint.waitForDeployment();
     console.log(`EntryPoint deployed to ${EntryPoint.target}`);
-
-    const ProjectDetails = await hre.ethers.deployContract("ProjectDetails");
-    await ProjectDetails.waitForDeployment();
-    console.log(`ProjectDetails deployed to ${ProjectDetails.target}`);
     
-    const accounts = await hre.ethers.deployContract("Accounts", [EntryPoint.target, ProjectDetails.target]);
-    await accounts.waitForDeployment();
-    console.log(`Accounts deployed to ${accounts.target}`);
-    
-    const paymaster = await hre.ethers.deployContract("Paymaster", [accounts.target]);
-    await paymaster.waitForDeployment();
-    console.log(`Paymaster deployed to ${paymaster.target}`);
-    
-    console.log("Creating paymaster stake in EntryPoint...");
-    const depositTx = await EntryPoint.depositTo(paymaster, {
-        value: hre.ethers.parseEther("0.2")
-    });
-    await depositTx.wait();
-    const balance = await EntryPoint.balanceOf(paymaster);
-    console.log("Paymaster stake in EntryPoint:", hre.ethers.formatEther(balance), "ETH");
-    
-
-    const addresses = {
-        EntryPoint: EntryPoint.target,
-        Accounts: accounts.target,
-        Paymaster: paymaster.target,
-        ProjectDetails: ProjectDetails.target,
-    }
-
-    fs.writeFileSync("addresses.json", JSON.stringify(addresses, null, 2));
-
     const Admin = await hre.ethers.deployContract("Admin");
     await Admin.waitForDeployment();
     console.log(`ProjectDetails deployed to ${Admin.target}`);
@@ -52,6 +22,34 @@ async function main(){
     await Milestone.waitForDeployment();
     console.log(`ProjectDetails deployed to ${Milestone.target}`);
     
+    const accounts = await hre.ethers.deployContract("Accounts", [EntryPoint.target, ProjectDetails.target]);
+    await accounts.waitForDeployment();
+    console.log(`Accounts deployed to ${accounts.target}`);
+    
+    const paymaster = await hre.ethers.deployContract("Paymaster", [accounts.target]);
+    await paymaster.waitForDeployment();
+    console.log(`Paymaster deployed to ${paymaster.target}`);
+    
+    console.log("Creating paymaster stake in EntryPoint...");
+    const depositTx = await EntryPoint.depositTo(paymaster, {
+        value: hre.ethers.parseEther("0.2")
+    });
+    await depositTx.wait();
+    const balance = await EntryPoint.balanceOf(paymaster);
+    console.log("Paymaster stake in EntryPoint:", hre.ethers.formatEther(balance), "ETH");
+
+    
+    const addresses = {
+        EntryPoint: EntryPoint.target,
+        Accounts: accounts.target,
+        Paymaster: paymaster.target,
+        Admin: Admin.target,
+        Validator: Validator,
+        Project: Project.target,
+        Milestone: Milestone.target
+    }
+
+    fs.writeFileSync("addresses.json", JSON.stringify(addresses, null, 2));
 }
 
 main().catch((error) => {
