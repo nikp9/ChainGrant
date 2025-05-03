15998c75983607ae6cf8fd56bb78ab8816ce0049
nikp9 <nikhilparihar0908@gmail.com>
2025-05-03 23:39:44 +0530

    Fix packages and hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,14 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.13",
+  defaultNetwork: "localhost",
+  solidity: {
+    version: "0.8.12",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000
+      }
+    }
+  }
 };
